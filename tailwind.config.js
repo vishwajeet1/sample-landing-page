a672ad9bdb3b5a446a194607f37197fb756677f0
vishwajeet <vishwajeet.rai@leapfinance.com>
2021-11-10 01:03:11 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      maxWidth: {
+        row: "1200px",
+        highRow: "1580px",
+        "75per": "75%",
+      },
+      colors: {
+        blue: {
+          light: "#89A7C3",
+          base: "#0E6FFF",
+        },
+        yellow: {
+          light: "#FFC928",
+        },
+        black: {
+          base: "#23242A",
+        },
+        pink: {
+          base: "#FABED7",
+        },
+        orange: {
+          base: "#FFAB74",
+        },
+      },
+      fontSize: {
+        font10: "10px",
+        font12: "12px",
+        font13: "13px",
+        font14: "14px",
+        font16: "16px",
+        font17: "17px",
+        font18: "18px",
+        font20: "20px",
+        font22: "22px",
+        font24: "24px",
+        font56: "56px",
+        font25: "25px",
+        font28: "28px",
+        font38: "38px",
+        font45: "45px",
+        font44: "44px",
+        font26: "26px",
+        font40: "40px",
+        font30: "30px",
+        font32: "32px",
+        font21: "21px",
+        font84: "84px",
+        font64: "64px",
+      },
+      width: {
+        w110: "110px",
+      },
+      minHeight: {
+        270: "270px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
